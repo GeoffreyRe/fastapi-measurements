bcd2ab01066f4ac390573c2126b852666bb6e1f7
Geoffrey Remacle <geoffrey.remacle@sneldev.com>
2025-05-05 15:02:04 +0200

    integrate Alembic to handle database migrations

--- app/db/database.py
+++ app/db/database.py
@@ -9,6 +9,9 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
 
+def get_alembic_url():
+    return SQLALCHEMY_DATABASE_URL
+
 def get_db():
     db = SessionLocal()
     try:
---
+++ app/models/__init__.py
@@ -0,0 +1,2 @@
+from .unit import Unit
+from .measurement import Measurement
